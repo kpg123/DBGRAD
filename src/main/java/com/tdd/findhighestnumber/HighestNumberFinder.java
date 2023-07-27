39ad2a6892aa81e586e71a943a1adbdcf75277cd
Kieran <kkgaffs@aol.com>
2023-07-27 20:54:30 +0100

    Fixed Code for test 3

--- src/main/java/com/tdd/findhighestnumber/HighestNumberFinder.java
+++ src/main/java/com/tdd/findhighestnumber/HighestNumberFinder.java
@@ -2,6 +2,12 @@ package com.tdd.findhighestnumber;
 
 public class HighestNumberFinder {
     public int findHighestNumber(int[] array) {
-        return array[0];
+        int max = array[0];
+        for(int i = 1; i < array.length; i++){
+            if (array[i] > max){
+                max = array[i];
+            }
+        }
+        return max;
     }
 }
